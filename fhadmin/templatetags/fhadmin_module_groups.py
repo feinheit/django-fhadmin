03a5ab72c722c46d6e3f7806b3f575956d6edbf7
Matthias Kestenholz <mk@spinlock.ch>
2014-05-13 14:59:12 +0200

    flake8-clean

--- fhadmin/templatetags/fhadmin_module_groups.py
+++ fhadmin/templatetags/fhadmin_module_groups.py
@@ -4,10 +4,9 @@ from django import template
 from django.conf import settings
 from django.contrib import admin
 from django.core.urlresolvers import reverse
-from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.text import capfirst
-from django.utils.translation import ugettext as _, ugettext_lazy
+from django.utils.translation import ugettext_lazy
 
 from fhadmin import FHADMIN_GROUPS_REMAINING
 
@@ -27,10 +26,10 @@ FHADMIN_GROUPS_CONFIG = [
     (ugettext_lazy('Collections'), {
         'apps': ('external', 'sharing', 'newsletter', 'form_designer'),
         }),
-    ]
+]
 
-FHADMIN_GROUPS_CONFIG = getattr(settings, 'FHADMIN_GROUPS_CONFIG',
-    FHADMIN_GROUPS_CONFIG)
+FHADMIN_GROUPS_CONFIG = getattr(
+    settings, 'FHADMIN_GROUPS_CONFIG', FHADMIN_GROUPS_CONFIG)
 
 
 def fhadmin_group_list(admin_site, request):
@@ -51,7 +50,8 @@ def fhadmin_group_list(admin_site, request):
             if True in perms.values():
                 model_dict = {
                     'name': capfirst(model._meta.verbose_name_plural),
-                    'admin_url': base_url + mark_safe('%s/%s/' % (app_label, model.__name__.lower())),
+                    'admin_url': base_url + mark_safe(
+                        '%s/%s/' % (app_label, model.__name__.lower())),
                     'perms': perms,
                 }
                 if app_label in app_dict:
@@ -62,7 +62,7 @@ def fhadmin_group_list(admin_site, request):
                         'app_url': base_url + app_label + '/',
                         'has_module_perms': has_module_perms,
                         'models': [model_dict],
-                        'app_label': app_label, # MK added this
+                        'app_label': app_label,  # MK added this
                     }
 
     # Sort the apps alphabetically.
@@ -75,7 +75,10 @@ def fhadmin_group_list(admin_site, request):
     # -- 8< --  copied from django.contrib.admin.sites.AdminSite.index
 
     all_available = [app['app_label'] for app in app_list]
-    all_configured = reduce(operator.add, (list(v['apps']) for k, v in FHADMIN_GROUPS_CONFIG), [])
+    all_configured = reduce(
+        operator.add,
+        (list(v['apps']) for k, v in FHADMIN_GROUPS_CONFIG),
+        [])
 
     all_remains = [a for a in all_available if a not in all_configured]
 
--- setup.py
+++ setup.py
@@ -5,15 +5,19 @@ from setuptools import setup, find_packages
 
 import fhadmin
 
-setup(name='django-fhadmin',
-      version=fhadmin.__version__,
-      description='Modifies the stock Django-Administration interface to fit our ideas a little bit better.',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-      author='Matthias Kestenholz',
-      author_email='mk@feinheit.ch',
-      url='http://github.com/feinheit/django-fhadmin/',
-      license='BSD License',
-      platforms=['OS Independent'],
-      packages=find_packages(),
-      include_package_data=True,
-      )
+setup(
+    name='django-fhadmin',
+    version=fhadmin.__version__,
+    description=(
+        'Modifies the stock Django-Administration interface'
+        ' to fit our ideas a little bit better.'),
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/feinheit/django-fhadmin/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+)
