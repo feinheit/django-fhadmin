1ed1425420b3a5cf37f516bf4bd3368524fb48ab
Matthias Kestenholz <mk@feinheit.ch>
2022-02-24 15:28:25 +0100

    Safety

--- fhadmin/static/fhadmin/fhadmin.js
+++ fhadmin/static/fhadmin/fhadmin.js
@@ -1,6 +1,5 @@
-/* global django */
 document.addEventListener("DOMContentLoaded", function () {
-  django.jQuery(function ($) {
+  window.django && window.django.jQuery && window.django.jQuery(function ($) {
     $("head").append(
       '<style type="text/css">#header h1:hover { cursor: pointer; }\x3C/style>'
     )
