984089c3e963998d62768721f23d7e7c72880e39
Matthias Kestenholz <mk@feinheit.ch>
2022-02-24 15:52:23 +0100

    Test the app list generation a bit

--- tests/testapp/test_fhadmin.py
+++ tests/testapp/test_fhadmin.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.contrib.auth.models import User
-from django.test import Client, TestCase
+from django.test import Client, RequestFactory, TestCase
+
+from fhadmin.templatetags.fhadmin_module_groups import generate_group_list
 
 
 class AdminTest(TestCase):
@@ -18,4 +21,15 @@ class AdminTest(TestCase):
         self.assertContains(response, "<h2>Modules</h2>")
         self.assertContains(response, "<h2>Preferences</h2>")
 
-        print(response, response.content.decode("utf-8"))
+        # print(response, response.content.decode("utf-8"))
+
+    def test_app_list(self):
+        request = RequestFactory().get("/")
+        request.user = User.objects.create(is_superuser=True)
+
+        groups = list(generate_group_list(admin.sites.site, request))
+        # from pprint import pprint; pprint(groups)
+
+        self.assertEqual(groups[0][0], "Modules")
+        self.assertEqual(groups[0][1][0]["app_label"], "testapp")
+        self.assertEqual(len(groups[0][1][0]["models"]), 1)
